608fd0a395dc6a825e26a18e58c2068832472c74
Yash Wasan <wasanyash25@gmail.com>
2021-08-20 22:38:03 +0530

    Updated 20-08-2021

--- ncert_solutions/class_10/science/index.php
+++ ncert_solutions/class_10/science/index.php
@@ -10,7 +10,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <!-- Favicon -->
       <link rel="shortcut icon" href="<?php echo $base_url; ?>img/favicon/favicon.ico" type="image/x-icon" />
-      <link rel="apple-touch-icon" href="<?php echo $base_url; ?>img/favicon/favicon.ico" />
+      <link rel="apple-touch-icon" href="<?php echo $base_url; ?>img/favicon/favicon.ico"/>
       <!-- Bootstrap CSS , CSS , Font Awesome-->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
       <link href="<?php echo $base_url; ?>/css/main.css" rel="stylesheet" />
